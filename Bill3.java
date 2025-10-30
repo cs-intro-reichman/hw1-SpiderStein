e80a328a57b8672106d3d2432b3482723c1aa722
Idan Steinberg <steinberg.idan@gmail.com>
2025-10-30 17:45:39 +0200

    fvcalc and bill3 RC

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		double bill = Double.parseDouble(args[3]);
+		double split = bill / 3;
+		System.out.printf("Dear %s, %s, and %s: pay %.1f Shekels each.%n", 
+		                  name1, name2, name3, split);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args) {
+		double investedSum = Double.parseDouble(args[0]);
+		double annualInterestRate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		double futureValue = investedSum * Math.pow(1 + annualInterestRate / 100, years);
+		System.out.printf("After %d years, $%.0f saved at %.1f%% will yield $%.0f%n", years, investedSum,
+				annualInterestRate, Math.floor(futureValue));
 	}
 }
