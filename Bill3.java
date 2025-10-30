95900d2c57ba124aeef4559814cdbb855bfde5bf
Idan Steinberg <steinberg.idan@gmail.com>
2025-10-30 17:51:17 +0200

    Bill3 RC

--- Bill3.java
+++ Bill3.java
@@ -2,9 +2,9 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-		String name1 = args[0];
+		String name1 = args[2];
 		String name2 = args[1];
-		String name3 = args[2];
+		String name3 = args[0];
 		double bill = Double.parseDouble(args[3]);
 		double split = bill / 3;
 		System.out.printf("Dear %s, %s, and %s: pay %.1f Shekels each.%n", 
