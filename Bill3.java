5f4ef8579588484a631b445a2f80fbb4f9cb6596
Idan Steinberg <steinberg.idan@gmail.com>
2025-10-30 17:55:35 +0200

    bill3 rc

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[0];
 		double bill = Double.parseDouble(args[3]);
-		double split = bill / 3;
+		double split = Math.ceil(bill / 3);
 		System.out.printf("Dear %s, %s, and %s: pay %.1f Shekels each.%n", 
 		                  name1, name2, name3, split);
 	}
